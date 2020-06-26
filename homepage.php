f3c133001c86409063204b2135ec3fa7a9316ba0
Emanuel Lourenco <emanuelwzp@hotmail.com>
2020-06-26 11:13:42 +0100

    Minor changes to layout

--- homepage.php
+++ homepage.php
@@ -4,7 +4,6 @@
   <meta charset="utf-8"/>
   <link rel="shortcut icon" href="favicon.ico"/>
   <title>Home Page</title>
-  <link rel="stylesheet" href="normalize.css"/>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css"/>
 </head>
@@ -21,17 +20,19 @@
         <?php include 'database_functions.php';?>
 
         <div class = row>
-          <h1 class="col-md-12">Testing database data insertion from user input</h1>
+          <div class ="col-md-12">
+          <h1 >Testing database data insertion from user input</h1>
 
           <form method="POST" class="col-md-6">
             <label for="username">Username:</label><br>
             <!--value should be gotten from _POST variable if user had input before-->
-            <input type="text" id="username" name="username" value=""><br>
+            <input type="text" id="username" name="username"  value="<?php if($_SERVER["REQUEST_METHOD"] === "POST"){echo $_POST["username"];}?>" required><br>
             <label for="pword">Password:</label><br>
             <input type="password" id="pword" name="pword" required><br><br>
             <input type="submit" value="Create User"><br>
             <span class="feedback"><?= $feedback;?></span>
           </form>
+        </div>
 
 
         </div>
