11005e58d9b65de209a7b39b7b746f0ee8adf558
Emanuel Lourenco <emanuelwzp@hotmail.com>
2020-06-27 09:39:34 +0100

    Fixes previous commit issues with favicon

--- submited.php
+++ submited.php
@@ -1,7 +1,8 @@
 <html lang="en">
 <head>
   <meta charset="utf-8"/>
-  <title>Home Page</title>
+  <link rel="shortcut icon" href="favicon.ico"/>
+  <title>Submission</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css"/>
 </head>
--- third_page.php
+++ third_page.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8"/>
-  <link rel="shortcut icon" href="Assets\favicon.ico"/>
+  <link rel="shortcut icon" href="favicon.ico"/>
   <title>Javascript Sandbox</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css"/>
