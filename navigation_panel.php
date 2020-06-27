af2f8f057cd0c9a81e1545811bb65e75dd1e278f
Emanuel Lourenco <emanuelwzp@hotmail.com>
2020-06-27 18:33:44 +0100

    Polishes navigation panel tags and css

--- navigation_panel.php
+++ navigation_panel.php
@@ -1,22 +1,26 @@
 <?php
 echo '
 
-<header class="row">
-  <div class="col-sm-2 col-md-1">
 
-  <img src="Assets\favicon.ico" alt="Logo" ></img>
+<header >
+  <div class = "row">
+    <div class="col-sm-2 col-md-1">
+      <img src="Assets\favicon.ico" alt="Logo" ></img>
+    </div>
 
-  </div>
-  <div class="col-sm-10 col-md-11">
     <nav>
-      <ul>
-        <li class = "col-sm-4 col-md-4"> <a href="homepage.php">Homepage</a></li>
-        <li class = "col-sm-4 col-md-4"> <a href="api_page.php">Advice With API</a></li>
-        <li class = "col-sm-4 col-md-4"> <a href="third_page.php">Other</a></li>
-      </ul>
+      <div class="col-sm-10 col-md-11">
+        <div class="row">
+          <a class = "col-sm-4 col-md-4" href="homepage.php" >Homepage</a>
+          <a class = "col-sm-4 col-md-4" href="api_page.php">Advice With API</a>
+          <button class="dropbtn col-sm-4 col-md-4">Dropdown
+            <i class="fa fa-caret-down"></i>
+          </button>
+        </div>
 
+      </div>
+    </div>
     </nav>
-  </div>
 
 </header>
 
